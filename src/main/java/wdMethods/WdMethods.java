0bda60efdaf49fdc657c4d2da0dc6188200d0c60
arjun0110 <u.arjunkumar@gmail.com>
2019-07-27 23:04:58 +0530

    Created the project structure that supports POM (Page Object Mode). It has 4 package - One to supports the test case, one to support the pages definition, one to support reading data from excel and reporting and the other one that takes all the selenium method definition. Added the chrome webdriver. Created the folder for screenshots and reports.

---
+++ src/main/java/wdMethods/WdMethods.java
@@ -0,0 +1,31 @@
+package wdMethods;
+
+import java.awt.AWTException;
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public interface WdMethods {
+		
+	
+	
+		public void startApp(String browser, boolean bReturns) throws MalformedURLException ;
+		public WebElement locateElement(String locator, String locValue) ;	
+		public WebElement locateElement(String locValue) ;	
+		public void type(WebElement ele, String data) ;
+		public void typeafterclearing(WebElement ele, String data) ;
+		public void click(WebElement ele);
+		public String getText(WebElement ele);
+		public void selectDropDownUsingText(WebElement ele, String value) ;
+		public void selectDropDownUsingIndex(WebElement ele, int index) ;
+		public boolean verifyTitle(String expectedTitle);
+		public void closeAllBrowsers();
+		
+		
+}
+
+
+
